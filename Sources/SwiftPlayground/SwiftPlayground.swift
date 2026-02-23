906ea8e6919e53bc236f37014fa2ca3c5bc29a35
oc-ericrattanak-nong-25 <ericrattanak.nong@student.onslow.school.nz>
2026-02-23 23:08:52 +0000

    IncompleteChallenge

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,57 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+//values
+
+let divider = "---------------"
+
+//boundaries 
+let maxEggStock = 1000
+let minEggStock = 0
+let minEggStockMinus1 = -1
+
+//array
+let menu = ["Add eggs", "Sell eggs", "Show current stock", "show total egg sold", "exit"]
+
+func menulist(){
+    var hi = true
+    var returnValue = -1
+while hi{ 
+    
+
+print("==== Egg Shop ====")
+menu.enumerated().forEach{index,menu in
+
+print("      \(index + 1 ) \(menu)")
+
+
+}   
+print(divider)
+print("Please enter a number:")
+print(divider)
+guard let userinput = readLine(),let number = Int(userinput), number > 0, number < 6 else {
+    print(divider)
+    print("Invalid input")
+    print(divider)
+    return menulist()
+}
+    returnValue = number
+    return returnValue
+if returnValue == 5 {
+    let hi = false
+
+} 
+}
+}
 @main
+
+
+
 struct SwiftPlayground {
+
     static func main() {
-        print("Hello, world!")
+
+menulist()
+
     }
 }
